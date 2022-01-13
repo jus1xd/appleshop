8f30e758de4c1c0cb181588613febdc92cf9b064
Vladislav <d1xys@bk.ru>
2022-01-13 19:16:12 +0300

    some changes

--- client/src/pages/compare.tsx
+++ client/src/pages/compare.tsx
@@ -1,22 +1,30 @@
 import React, { useEffect, useState } from "react";
-import BasketCard from "../components/BasketCard/BasketCard";
-import DateItem from "../components/DateItem/DateItem";
-import TimeItem from "../components/TimeItem/TimeItem";
 import Header from "../components/Header/Header";
-import Modal from "../components/Modal/Modal";
 import { useAppDispatch, useAppSelector } from "../hooks/redux";
 import s from "../styles/Compare.module.css";
 import { getUserCart } from "../store/actions/fetchProducts";
 import jwtDecode from "jwt-decode";
+import CompareCard from "../components/CompareCard/CompareCard";
 
 const Compare = () => {
+
+  const dispatch = useAppDispatch();
+
+
   return (
     <>
       <Header />
       <div className={s.compare}>
         <div className={s.container}>
           <div className={s.section_inner}>
-            <div className={s.section_title}>Сравнить</div>
+            <div className={s.section_header}>
+              <div className={s.section_title}>Сравнить</div>
+              {/* <div className={s.compare_btn}>Показать различия</div> */}
+            </div>
+            {/* <div className={s.product_cards}>
+              <CompareCard  />
+            </div> */}
+
             <div className={s.error_wrapper}>
               <div className={s.error_icon}>(o^^)o</div>
               <div className={s.error_title}>Нечего сравнивать</div>
